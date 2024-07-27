5aeeedfb85e081aa3cb5c44e832542f34e8bab02
marwanchtw240 <marwanchtw240@gmail.com>
2024-07-28 00:43:18 +0100

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import HeaderTop from "@/Components/HeaderTop";
+import HeaderMain from "@/Components/HeaderMain";
+import NavBar from "@/Components/NavBar";
+import MobNavbar from "@/Components/MobNavbar";
+
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Footer from "@/Components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +20,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <HeaderTop/>
+        <HeaderMain/>
+        <NavBar/>
+        <MobNavbar/>
+        {children}
+        <Footer/>
+        </body>
     </html>
   );
 }
