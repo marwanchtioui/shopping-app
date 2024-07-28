5c507ead2db8d0fed1eac620a39537cba642ba6e
marwanchtw240 <marwanchtw240@gmail.com>
2024-07-28 16:28:56 +0100

    updates paths

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -10,7 +10,7 @@ const Testimonial = () => {
             <div className='grid lg:grid-cols-[300px,1fr] gap-4'>
                 <div className='border border-gray-300 rounded-2xl grid place-items-center p-6 lg:p-0'>
                     <div className='text-center flex flex-col items-center gap-1'>
-                        <Image className='rounded-full inline-block' src="/user.jpg" width={80} height={80} alt="profile"/>
+                        <Image className='rounded-full inline-block' src="/shopping-app/user.jpg" width={80} height={80} alt="profile"/>
                         <h2 className='text-gray-500 font-black text-[20px]'>Elsa Doe</h2>
                         <p>CEO & Founder Invision</p>
                         <Image className='inline-block py-2' src="/quotes.svg" width={30} height={30} alt="quotes"/>
@@ -20,7 +20,7 @@ const Testimonial = () => {
                     </div>
                 </div>
 
-                <div className='bg-red-600 bg-[url(/cta-banner.jpg)] bg-cover h-[500px] rounded-2xl grid place-items-center'>
+                <div className='bg-red-600 bg-[url(/shopping-app/cta-banner.jpg)] bg-cover h-[500px] rounded-2xl grid place-items-center'>
                     <div className='bg-[#ffffffab] min-w-[270px] sm:min-w-[300px] md:min-w-[500px] rounded-xl py-8 sm:px-8 px-2 grid place-items-center gap-3'>
                         <button className='bg-blackish text-white p-2 rounded-md'>25% DISCOUNT</button>
                         <h2 className='font-extrabold text-2xl text-[#272727]'>Summer Collection</h2>
